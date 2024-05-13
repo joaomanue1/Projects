ca5b1cf98d90692ae616124a32e51e0202733b80
João Manuel <joaomanuel@Laptop-de-Joao.local>
2024-05-13 09:49:29 -0300

    first commit

---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,13 @@
+package principal;
+
+import visao.FrmMenuPrincipal;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        FrmMenuPrincipal objetotela = new FrmMenuPrincipal();
+
+        objetotela.setVisible(true);
+    }
+}
